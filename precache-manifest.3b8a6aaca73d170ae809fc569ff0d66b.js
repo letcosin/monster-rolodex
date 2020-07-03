c7978dfc10c1e60df8db0c616c1adbc1fc22b722
Leticia Cosin <leticia.cosin@hotmail.com>
2020-07-02 22:57:57 -0300

    Updates

---
+++ precache-manifest.3b8a6aaca73d170ae809fc569ff0d66b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea0ba7d6be42fee4d00c2d5ad71fa26",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1227e182bffb8f3f9923",
+    "url": "/monster-rolodex/static/css/main.c6485797.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1227e182bffb8f3f9923",
+    "url": "/monster-rolodex/static/js/main.5d5382ae.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
